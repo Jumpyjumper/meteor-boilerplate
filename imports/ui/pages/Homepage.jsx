184ec037c0fd113596146011174ec8e849a1ca12
fabien <fmaronnaud@klick.com>
2018-12-09 13:47:17 -0500

    page(home): add home page

---
+++ imports/ui/pages/Homepage.jsx
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import Alert from '/imports/ui/components/alert/alert.jsx';
+import Badge from '/imports/ui/components/badge/badge.jsx';
+import { H1, H2, H3, H4, H5, H6 } from '/imports/ui/components/headings/headings.jsx';
+import { Table, Thead, Tbody, Tr, Th, Td } from '/imports/ui/components/table/table.jsx';
+import Caption from '/imports/ui/components/caption/caption.jsx';
+
+export default class Homepage extends React.Component {
+
+	constructor(props) {
+		super(props);
+    }
+
+    componentDidMount(){
+    }
+   
+	render() {
+		return (
+            <div className="container-fluid">				
+			</div>		
+		)
+	}
+
+}
+
