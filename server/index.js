a59147f1a41aff4ce971ed4df0b368b8c9a514fc
fabien <fmaronnaud@klick.com>
2018-12-13 21:38:36 -0500

    wip(favicons)

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
+import '/imports/startup/server';
 import {WebApp} from 'meteor/webapp';
 
+var favicons = JSON.parse(Assets.getText('favicons.json'));
+var faviconslist = favicons.join("\n");
+
 WebApp.addHtmlAttributeHook(() => ({lang: 'en'}));
+
+WebApp.rawConnectHandlers.use(
+    Meteor.bindEnvironment(function (req, res, next) {
+        req.dynamicHead = (req.dynamicHead || "") + faviconslist;
+        next();
+    })
+);
