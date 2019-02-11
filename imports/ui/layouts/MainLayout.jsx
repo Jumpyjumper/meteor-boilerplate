d603ac8057a91cb817ed116c9f98251925e88ffb
fabien <fmaronnaud@klick.com>
2019-02-10 22:31:44 -0500

    fix(mainlayout): revert to flowrouter type layout

--- imports/ui/layouts/MainLayout.jsx
+++ imports/ui/layouts/MainLayout.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Breadcrumbs from '/imports/ui/components/breadcrumbs/breadcrumbs.jsx';
+import MDWebsite from '/imports/ui/components/microdata/website.jsx';
 
-const App = ({ children }) => (
-    <div typeof="schema:WebPage">     
-        <main property="schema:mainContentOfPage">
-            {children}
+const MainLayout = ({content}) => (
+    <div>
+        <MDWebsite/>
+        <header>
+        This is our header
+        </header>
+        <Breadcrumbs breadcrumbs={Breadcrumb.getAll()} />
+        <main>
+            {content()}
         </main>
     </div>
 );
 
-export default App;
+MainLayout.propTypes = {
+    content: PropTypes.func
+};
 
+export {
+    MainLayout
+};
