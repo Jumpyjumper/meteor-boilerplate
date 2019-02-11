ac9ab43c41a43bd36b0250f19e287ea31be57a24
fabien <fmaronnaud@klick.com>
2019-02-10 22:30:06 -0500

    fix(client): update client imports

--- client/index.js
+++ client/index.js
@@ -1,2 +1,1 @@
 import '/imports/startup/client';
-import './routes/index.js';
--- client/index.test.js
+++
@@ -1,6 +0,0 @@
-after(function() {
-    Meteor.sendCoverage(function(){});
-    Meteor.exportCoverage("coverage", function(err) {console.log(err)})
-    Meteor.exportCoverage("json", function(err) {console.log(err)})
-    Meteor.exportCoverage("lcovonly", function(err) {console.log(err)})
-});
