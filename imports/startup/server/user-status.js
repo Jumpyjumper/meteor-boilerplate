a55097fa78e34ed10990acead5ad1203c01f3adf
Jumpyjumper <fmaronnaud@klick.com>
2019-06-10 15:35:34 -0400

    feat(fontawesome + platform): add fontawesome5 react and platform packages

--- imports/startup/server/user-status.js
+++ imports/startup/server/user-status.js
@@ -2,8 +2,8 @@
 UserPresence.start();
 
 if(Meteor.isDevelopment){
-	// Active logs for every changes
-	UserPresence.activeLogs();
+    // Active logs for every changes
+    UserPresence.activeLogs();
 }
 
 // Listen for changes in UserSessions and Meteor.users to set user status based on active connections
